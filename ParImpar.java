2bcc8e9d80139738ce9d9280b87db46e454f0fd5
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-28 17:55:41 -0600

    Terminado. Agregadas ventanas de dialogo de entrada y salida y mensaje de error

--- ParImpar.java
+++ ParImpar.java
@@ -1,11 +1,22 @@
+import javax.swing.JOptionPane;
+
 public class ParImpar {
     public static void main(String[] args) {
-            Double i = 486.0;
-            if(i % 2 == 0){
-                System.out.println("Es un numero par");
-            } else {
-                System.out.println("Es un numero impar");
-            }
+
+        String numStr = JOptionPane.showInputDialog(null, "Ingrese un numero para saber si es par o impar");
+        double num1 = 0;
+        try {
+            num1 = Double.parseDouble(numStr);
+        } catch (NumberFormatException e){
+           JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+        if(num1 % 2 == 0){
+            JOptionPane.showMessageDialog(null, "Es un numero par");
+        } else {
+            JOptionPane.showMessageDialog(null, "Es un numero impar");
+        }
     }
     
 }
