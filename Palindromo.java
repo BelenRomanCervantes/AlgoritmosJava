ec9e0d62f5b32579825d47b06bed45b712371a5b
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-28 21:27:49 -0600

    Creacion de Palindromo. Incompleto

---
+++ Palindromo.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class Palindromo {
+    public static void main(String[] args) {
+        
+        //Ventana de dialogo para solicitar la entrada de datos
+        String cadena = JOptionPane.showInputDialog(null, "Introduzca una palabra o frase para saber si es un palindromo");
+        
+        //Eliminacion de los espacios al inicio y final
+        String cadena2 = cadena.trim();
+
+        //Eliminacion de espacios intermedios
+        String cadena3 = 
+
+        //Eliminacion de caracteres especiales
+
+        //Comparacion de la primera y ultima mitad
+        
+
+    }
+    
+}
