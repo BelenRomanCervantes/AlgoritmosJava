4137533160913f88be7a4dc76ab9d31fec05b07a
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-28 21:26:45 -0600

    Update OrdenarNumeros. Falta corregir ciclo

--- OrdenarNumeros.java
+++ OrdenarNumeros.java
@@ -1,14 +1,72 @@
+import javax.swing.JOptionPane;
+
 public class OrdenarNumeros {
     public static void main(String[] args) {
-        double a = 4.5;
-        double b = -6.3;
-        double c = 75.64;
-        double d = 3.1416;
-        double e = 65.0;
 
+        //Ventana de dialogo para recibir datos de entrada
+        String numStr1 = JOptionPane.showInputDialog(null, "Ingrese un numero");
+        double num1 = 0;
+        try {
+            num1 = Double.parseDouble(numStr1);
+        } catch (NumberFormatException e){
+           JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+        String numStr2 = JOptionPane.showInputDialog(null, "Ingrese un segundo numero");
+        double num2 = 0;
+        try {
+            num2 = Double.parseDouble(numStr2);
+        } catch (NumberFormatException e){
+           JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+        String numStr3 = JOptionPane.showInputDialog(null, "Ingrese un tercer numero");
+        double num3 = 0;
+        try {
+            num3 = Double.parseDouble(numStr3);
+        } catch (NumberFormatException e){
+           JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+        String numStr4 = JOptionPane.showInputDialog(null, "Ingrese un cuarto numero");
+        double num4 = 0;
+        try {
+            num4 = Double.parseDouble(numStr4);
+        } catch (NumberFormatException e){
+           JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+        String numStr5 = JOptionPane.showInputDialog(null, "Ingrese un quinto numero");
+        double num5 = 0;
+        try {
+            num5 = Double.parseDouble(numStr5);
+        } catch (NumberFormatException e){
+           JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+
+
+        //Ciclo para ordenar los numeros
+        double nuevo = 0;
+        for(int i = 1; i == 5; i++){
+            if(i == 1){
+                double a = nuevo;
+            } else (i == 2) {
+                double b = a;
+                
+
+
+            }
+        }
         
-
         String listaOrdenada = a + " " + b + " " + c + " " + d + " " + e;
-        System.out.println(listaOrdenada);
+        
+        //Ventana de dialogo de salida de datos
+        JOptionPane.showMessageDialog(null, listaOrdenada);
     }
 }
