e4321addce423dfd5e37298525f83cf6a85b5f8b
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-28 21:20:37 -0600

    Creacion DiasFaltantes. Falta corregir: arroja negativos

---
+++ DiasFaltantes.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;
+
+public class DiasFaltantes {
+    public static void main(String[] args) {
+        
+        //Ventana de dialogo para recibir datos de entrada
+        String dActStr = JOptionPane.showInputDialog(null, "Ingrese el dia actual (DD)");
+        int dAct = 0;
+        try {
+            dAct = Integer.parseInt(dActStr);
+        } catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+        String mActStr = JOptionPane.showInputDialog(null, "Ingrese el mes actual (MM)");
+        int mAct = 0;
+        try {
+            mAct = Integer.parseInt(mActStr);
+        } catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+
+        //Calculo de los dias faltantes ***Corregir: arroja negativos***
+        int diasHastaHoy = (mAct * 30) + (dAct * 1);
+        int diasParaMuertos = 331 - diasHastaHoy;
+        String resultadoMuertos = "Los dias que faltan hasta dia de muertos son: " + diasParaMuertos;
+        int diasParaNavidad = 385 - diasHastaHoy;
+        String resultadoNavidad = "Los dias faltantes para Navidad son: " + diasParaNavidad;
+
+        //Ventana de dialogo de salida
+        JOptionPane.showMessageDialog(null, resultadoMuertos);
+        JOptionPane.showMessageDialog(null, resultadoNavidad);
+
+
+
+    }
+    
+}
