bf1d91757e8461ca19114996257c21d8a90de52f
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-28 21:18:18 -0600

    Update CalculoEsfera. Terminado

--- CalculoEsfera.java
+++ CalculoEsfera.java
@@ -3,19 +3,25 @@ import javax.swing.JOptionPane;
 public class CalculoEsfera {
     public static void main(String[] args) {
     
+        //Ventana de dialogo para recibir datos de entrada
         String radioStr = JOptionPane.showInputDialog(null, "Ingrese el radio de la esfera en cm");
         double radioDou = 0;
+        
+        //Prueba para marcar error en caso de recibir una entrada incorrecta
         try {
             radioDou = Double.parseDouble(radioStr);
         } catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero positivo");
+            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
             main(args);
             System.exit(0);
         }
 
-        
+        //Calculo del volumen de la esfera
         double volumenEsfera = ((Math.pow(radioDou, 3)) * Math.PI * 4) /3;
+        
+        //Ventana de dialogo del resultado
         String resultado = "El volumen de la esfera es: " + volumenEsfera + " cm3";
         JOptionPane.showMessageDialog(null, resultado);
+        
     }
 }
