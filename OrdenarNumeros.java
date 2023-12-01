a1884ae5d087bd26a860f97fe58c90949c41d2cb
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-30 21:32:23 -0600

    Update OrdenarNumeros. Pendiente

--- OrdenarNumeros.java
+++ OrdenarNumeros.java
@@ -1,72 +1,89 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class OrdenarNumeros {
     public static void main(String[] args) {
 
-        //Ventana de dialogo para recibir datos de entrada
+        /*Ventana de dialogo para recibir datos de entrada
         String numStr1 = JOptionPane.showInputDialog(null, "Ingrese un numero");
-        double num1 = 0;
+        int num1 = 0;
         try {
-            num1 = Double.parseDouble(numStr1);
+            num1 = Integer.parseInt(numStr1);
         } catch (NumberFormatException e){
            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
             main(args);
             System.exit(0);
         }
         String numStr2 = JOptionPane.showInputDialog(null, "Ingrese un segundo numero");
-        double num2 = 0;
+        int num2 = 0;
         try {
-            num2 = Double.parseDouble(numStr2);
+            num2 = Integer.parseInt(numStr2);
         } catch (NumberFormatException e){
            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
             main(args);
             System.exit(0);
         }
         String numStr3 = JOptionPane.showInputDialog(null, "Ingrese un tercer numero");
-        double num3 = 0;
+        int num3 = 0;
         try {
-            num3 = Double.parseDouble(numStr3);
+            num3 =Integer.parseInt(numStr3);
         } catch (NumberFormatException e){
            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
             main(args);
             System.exit(0);
         }
         String numStr4 = JOptionPane.showInputDialog(null, "Ingrese un cuarto numero");
-        double num4 = 0;
+        int num4 = 0;
         try {
-            num4 = Double.parseDouble(numStr4);
+            num4 = Integer.parseInt(numStr4);
         } catch (NumberFormatException e){
            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
             main(args);
             System.exit(0);
         }
         String numStr5 = JOptionPane.showInputDialog(null, "Ingrese un quinto numero");
-        double num5 = 0;
+        int num5 = 0;
         try {
-            num5 = Double.parseDouble(numStr5);
+            num5 = Integer.parseInt(numStr5);
         } catch (NumberFormatException e){
            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
             main(args);
             System.exit(0);
         }
+        */
+
+        //Creacion de un arreglo con los datos de entrada
+        int array[] = new int[4];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero");
+        String num1Str = scanner.nextLine();
+        array[1] = 0;
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero");
+        String num2Str = scanner.nextLine();
+        array[2] = 0;
 
 
         //Ciclo para ordenar los numeros
-        double nuevo = 0;
-        for(int i = 1; i == 5; i++){
-            if(i == 1){
-                double a = nuevo;
-            } else (i == 2) {
-                double b = a;
-                
-
-
+        for (int i = 0; i < array.length; i++) {
+            for (int nuevo = i + 1; nuevo < array.length; nuevo++) {
+                if (array[nuevo] < array[i]){
+                    int aux = array[nuevo];
+                    array[nuevo] = array[i];
+                    array[i] = aux;
+                }
             }
         }
-        
-        String listaOrdenada = a + " " + b + " " + c + " " + d + " " + e;
-        
+
+        //Ciclo para iterar sobre los elementos del array
+        for(int i = 0; i < array.length; i++){
+        System.out.println(array[i]);
+        }
+        //String listaOrdenada = Arrays.toString(array[i]);
+
         //Ventana de dialogo de salida de datos
-        JOptionPane.showMessageDialog(null, listaOrdenada);
+        //JOptionPane.showMessageDialog(null, listaOrdenada);
     }
 }
