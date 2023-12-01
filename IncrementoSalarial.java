a7e406784cc2c2ab62b2e88367911ff6b1cff337
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-11-30 18:29:53 -0600

    Creacion del algoritmo IncrementoSalarial. Terminado

---
+++ IncrementoSalarial.java
@@ -0,0 +1,32 @@
+import javax.swing.JOptionPane;
+
+public class IncrementoSalarial {
+    public static void main(String[] args) {
+        
+        //Ventana de dialogo para la entrada de datos
+        String salActualStr = JOptionPane.showInputDialog(null, "Ingresa tu salario actual");
+        double salActual = 0;
+        
+        //Prueba para marcar error en caso de recibir una entrada incorrecta
+        try {
+            salActual = Double.parseDouble(salActualStr);
+        } catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            main(args);
+            System.exit(0);
+        }
+
+        //Calculo del incremento
+        double salNuevo = 0.0;
+        if(salActual <= 15000){
+            salNuevo = salActual * 1.20;
+        } else {
+            salNuevo = salActual * 1.15;
+        }
+
+        //Mensaje de salida
+        String respuesta = "Tu salario actualizado es: " + salNuevo;
+        JOptionPane.showMessageDialog(null, respuesta);
+    }
+    
+}
