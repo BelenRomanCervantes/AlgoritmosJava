f53349d1938947d2757eca55d41a2755b891655d
Belén Itzel Román Cervantes <124020263+BelenRomanCervantes@users.noreply.github.com>
2023-12-03 23:59:12 -0600

    Update DiasFaltantes. Calculo listo. Solo falta considerar año bisiesto

--- DiasFaltantes.java
+++ DiasFaltantes.java
@@ -1,53 +1,99 @@
-import java.time.LocalDate;
-
 import javax.swing.JOptionPane;
 
 public class DiasFaltantes {
     public static void main(String[] args) {
         
-        /*Ventana de dialogo para recibir datos de entrada
-        String dActStr = JOptionPane.showInputDialog(null, "Ingrese el dia actual (DD)");
+        //Ventana de dialogo para recibir datos de entrada
+        String dActStr = JOptionPane.showInputDialog(null, "Ingrese el NUMERO del DIA actual");
         int dAct = 0;
         try {
             dAct = Integer.parseInt(dActStr);
         } catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            JOptionPane.showMessageDialog(null, "Error!\nDebe ingresar un NUMERO");
             main(args);
             System.exit(0);
         }
-        String mActStr = JOptionPane.showInputDialog(null, "Ingrese el mes actual (MM)");
+        String mActStr = JOptionPane.showInputDialog(null, "Ingrese el NUMERO del MES actual");
         int mAct = 0;
         try {
             mAct = Integer.parseInt(mActStr);
         } catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(null, "Error: Debe ingresar un numero");
+            JOptionPane.showMessageDialog(null, "Error!\nDebe ingresar un NUMERO");
             main(args);
             System.exit(0);
         }
-        */
 
-        LocalDate fechaAct = LocalDate.now();
-        LocalDate diaMuertos = LocalDate.parse("Dia de muertos", formatter);
-        
-
-
-
-        /*Calculo de los dias faltantes ***Corregir: arroja negativos***
-        if(mAct >= 11){
-
+        //Calculo de los dias que han pasado hasta dAct
+        int diasHastaHoy = 0;
+        switch (mAct) {
+            case 1: 
+                diasHastaHoy = dAct + 0;
+                break;
+            case 2: 
+                diasHastaHoy = dAct + 31;
+                break;
+            case 3: 
+                diasHastaHoy = dAct + 31 + 28;
+                break;
+            case 4:
+                diasHastaHoy = dAct + 31 + 28 + 31;
+                break;
+            case 5:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30;
+                break;
+            case 6:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31;
+                break;
+            case 7:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31 + 30; 
+                break;   
+            case 8:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31 + 30 + 31;
+                break; 
+            case 9:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+                break;
+            case 10:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+                break;
+            case 11:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+                break;
+            case 12:
+                diasHastaHoy = dAct + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+                break;
         }
-        int diasHastaHoy = (mAct * 30) + (dAct * 1);
-        int diasParaMuertos = 331 - diasHastaHoy;
-        String resultadoMuertos = "Los dias que faltan hasta dia de muertos son: " + diasParaMuertos;
-        int diasParaNavidad = 385 - diasHastaHoy;
-        String resultadoNavidad = "Los dias faltantes para Navidad son: " + diasParaNavidad;
 
-        //Ventana de dialogo de salida
-        JOptionPane.showMessageDialog(null, resultadoMuertos);
-        JOptionPane.showMessageDialog(null, resultadoNavidad);
-*/
+        //Calculo de excepciones (Dia de muertos)
+        int diasParaMuertos = 0;
+        if(mAct == 11 && dAct == 1){
+            JOptionPane.showMessageDialog(null, "Felicidades!! Hoy es DIA DE MUERTOS");
+        } else if(mAct == 11 && dAct > 1){
+            diasParaMuertos = 365 - dAct;
+            JOptionPane.showMessageDialog(null, "Faltan: " + diasParaMuertos + " dias para DIA DE MUERTOS");
+        } else if(mAct == 12){
+            diasParaMuertos = 365 - (29 + dAct);
+            JOptionPane.showMessageDialog(null, "Faltan: " + diasParaMuertos + " dias para DIA DE MUERTOS");
+        } else {
+            diasParaMuertos = 331 - diasHastaHoy;
+            JOptionPane.showMessageDialog(null, "Faltan: " + diasParaMuertos + " dias para DIA DE MUERTOS");
+        }
 
-
+        //Calculo de excepciones (Navidad)
+        int diasParaNavidad = 0;
+        if(mAct == 12 && dAct == 24){
+            JOptionPane.showMessageDialog(null, "Felicidades!! Hoy es NAVIDAD");
+        } else if(mAct == 12 && dAct > 24){
+            diasParaNavidad = 365 - dAct;
+            JOptionPane.showMessageDialog(null, "Faltan: " + diasParaNavidad + " dias para NAVIDAD");
+        } else {
+            diasParaNavidad = 358 - diasHastaHoy;
+            JOptionPane.showMessageDialog(null, "Faltan: " + diasParaNavidad + " dias para NAVIDAD");
+        }
+        
     }
-    
+
+
 }
+    
+
